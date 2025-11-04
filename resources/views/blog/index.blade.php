44e5b4ff26eb95fe85a3fe0e274f6f4e430cbe52
Ajitsinghoccultscience <ajitsingh.occultscience@gmail.com>
2025-11-04 12:30:54 +0530

    image

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -52,15 +52,19 @@ use Illuminate\Support\Facades\Storage;
                                 @endif
                             </div>
                                                 <!-- Badge -->
-                                                <div class="absolute top-3 left-3">
+                                                <div class="absolute top-3 right-3">
                                                     <span class="bg-orange-600 text-white text-xs font-bold px-3 py-1 rounded-sm shadow-md flex items-center">
                                                         <svg class="w-3 h-3 mr-1" fill="currentColor" viewBox="0 0 20 20">
                                                             <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
                                                         </svg>
-                                                        Occult Science
+                                                        @if($post->category && $post->category->name)
+                                                            {{ $post->category->name }}
+                                                        @else
+                                                            Uncategorized
+                                                        @endif
                                                     </span>
-                            </div>
-                                                </a>
+                                                </div>
+                                              </a>
                                             </div>
                                             
                                             <!-- Post Content -->

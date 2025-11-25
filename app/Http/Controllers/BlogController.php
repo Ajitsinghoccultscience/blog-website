e57356336e33fa9b644c13fd23597acb53fd0756
Ajitsinghoccultscience <ajitsingh.occultscience@gmail.com>
2025-11-25 11:56:34 +0530

    blog

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -87,6 +87,10 @@ class BlogController extends Controller
 
         // Set meta data
         $metaTitle = $post->meta_title ?: $post->title;
+        // Remove "Occult Science Blog" or " - Occult Science Blog" from title
+        $metaTitle = preg_replace('/\s*-\s*Occult Science Blog\s*/i', '', $metaTitle);
+        $metaTitle = preg_replace('/\s*Occult Science Blog\s*-\s*/i', '', $metaTitle);
+        $metaTitle = trim($metaTitle);
         $metaDescription = $post->meta_description ?: $post->excerpt ?: Str::limit(strip_tags($post->content), 160);
 
         // Process content to fix image URLs

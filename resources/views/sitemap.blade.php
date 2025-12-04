8aea6e611651731ecc60c230a3efd3738da9645c
Ajitsinghoccultscience <ajitsingh.occultscience@gmail.com>
2025-12-04 12:04:47 +0530

    new

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,31 +1,22 @@
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9             http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+<url>
+<loc>{{ url('/') }}/</loc>
 
-    {{-- Homepage --}}
-    <url>
-        <loc>{{ url('/') }}/</loc>
-    </url>
 
-    {{-- About Page --}}
-    <url>
-        <loc>{{ route('blog.about') }}/</loc>
-    </url>
+</url>
+<url>
+<loc>{{ route('blog.about') }}/</loc>
 
-    {{-- All Published Blog Posts --}}
-    @foreach ($posts as $post)
-    @if(!empty($post->slug))
-    <url>
-        <loc>{{ route('blog.post', $post->slug) }}/</loc>
-    </url>
-    @endif
-    @endforeach
 
-    {{-- All Active Categories - Commented out for now, will use in future --}}
-    {{-- @foreach ($categories as $category)
-    <url>
-        <loc>{{ route('blog.category', $category->slug) }}/</loc>
-        <lastmod>{{ $category->updated_at->toAtomString() }}</lastmod>
-    </url>
-    @endforeach --}}
-    
+</url>
+@foreach ($posts as $post)
+@if(!empty($post->slug))
+<url>
+<loc>{{ route('blog.post', $post->slug) }}/</loc>
+
+
+</url>
+@endif
+@endforeach
 </urlset>
 

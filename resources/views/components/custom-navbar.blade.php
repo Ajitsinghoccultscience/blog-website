e0d0627230d9525c8d7279fc675780c6dc2eab6f
Ajitsinghoccultscience <ajitsingh.occultscience@gmail.com>
2026-07-03 11:51:25 +0530

    new changes

--- resources/views/components/custom-navbar.blade.php
+++ resources/views/components/custom-navbar.blade.php
@@ -9,7 +9,7 @@
                     <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20">
                         <path fill-rule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd"></path>
                     </svg>
-                    <span>08 / 20 South Patel Nagar New Delhi - 110008</span>
+                    <span>7/25, Plot-A, Kirti Nagar Industrial Area, Kirti Nagar, New Delhi - 110015</span>
                 </div>
                 
                 <!-- Right Side - Social Media Icons -->
